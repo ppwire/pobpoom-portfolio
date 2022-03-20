fa828b13dde779ada6536d652cc8b484e8cbcc02
Poom Muangthai <poom.mth@hotmail.com>
2022-03-20 23:31:27 +0700

    Update React to about section

--- components/about.js
+++ components/about.js
@@ -5,7 +5,7 @@ function About() {
       <VStack spacing={4}>
          <Text bg="secondary" p={3} borderRadius="8" textAlign="center" fontFamily="Open Sans">
             Hi! I am Poom Muangthai. I am currently looking for work.
-            Mostly thing that I work on is Node and Vue.
+            Mostly thing that I work on is Node, React and Vue.
          </Text>
          <Button as={Link} href="https://bit.ly/3MYyM9D" bg="primary" rightIcon={<ChevronRightIcon />} colorScheme="red">
             Resume
